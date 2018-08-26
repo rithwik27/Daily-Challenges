48b676e27bd7acbb6177fe5c6629a407f4884c8f
rithwik27 <itsme.rithwik@gmail.com>
2018-08-26 21:57:16 +0530

    Daily challenges

--- Daily Challenges/codeforces/3-palindrom.cpp
+++ Daily Challenges/codeforces/3-palindrom.cpp
@@ -1,14 +1,14 @@
-#include<string.h> 
+#include<string.h>
 #include<iostream>
 #include<stdio.h>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	int t,x,y;
+    int t,x,y;
     string n="aabb";
-	cin >> t;
+    cin >> t;
     x=t/4;y=t%4;
     while (x--) cout << n;
-    if(y>0) cout << n.substr(0, y-1);
+    if(y>0) cout << n.substr(0, y);
     
 }
